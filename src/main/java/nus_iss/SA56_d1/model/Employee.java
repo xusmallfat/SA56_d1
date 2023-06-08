f43b1fd35e1ac36d2049caf8e8a2668433506125
weicongsoh <wsu1@e.ntu.edu.sg>
2023-06-08 15:24:20 +0800

    first commit

---
+++ src/main/java/nus_iss/SA56_d1/model/Employee.java
@@ -0,0 +1,16 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Employee {
+    private String id;
+    private String firstName;
+    private String lastName;
+   
+    
+}
