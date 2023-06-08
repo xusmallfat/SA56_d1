b64f6661407ddd5d715ad09d2a53b26c2ed8966f
weicongsoh <wsu1@e.ntu.edu.sg>
2023-06-08 14:58:27 +0800

    first commit

---
+++ src/main/java/nus_iss/SA56_d1/Sa56D1Application.java
@@ -0,0 +1,13 @@
+package nus_iss.SA56_d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa56D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa56D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/nus_iss/SA56_d1/Sa56D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package nus_iss.SA56_d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa56D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
